dbe460622bd6ed741c1542edb904eae9393090db
Ganesh <148611845+ganesh-idhole@users.noreply.github.com>
2024-09-05 16:29:38 +0530

    Update config.js

--- Backend/db/config.js
+++ Backend/db/config.js
@@ -1,2 +1,21 @@
+// const mongoose = require('mongoose');
+// mongoose.connect("mongodb://localhost:27017/hostel");
+
 const mongoose = require('mongoose');
-mongoose.connect("mongodb://localhost:27017/hostel");
+const dotenv = require('dotenv');
+
+// Load environment variables from .env file
+dotenv.config();
+
+const dbURI = process.env.DB_URI;
+
+if (!dbURI) {
+    throw new Error('MongoDB connection string is missing in environment variables.');
+}
+
+mongoose.connect(dbURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+    .then(() => console.log('Successfully connected to MongoDB'))
+    .catch(err => console.error('Error connecting to MongoDB:', err));
