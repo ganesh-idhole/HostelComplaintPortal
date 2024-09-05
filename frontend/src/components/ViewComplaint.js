dfb6b6c1bbf98ec720fcc042c453bbb643d722a1
Ganesh <148611845+ganesh-idhole@users.noreply.github.com>
2024-09-05 18:12:16 +0530

    Update ViewComplaint.js

--- frontend/src/components/ViewComplaint.js
+++ frontend/src/components/ViewComplaint.js
@@ -19,20 +19,20 @@ const ViewComplaint = ()=>{
     },[])
      
     const viewusers = async () =>{
-        let result =  await fetch("http://localhost:5000/view");
+        let result =  await fetch("https://hostelcomplaintportalbackend1.onrender.com/view");
         result = await result.json();
         setUsers(result);
     }
 
     const DeleteForAdmin = async(id,deleteforstudent)=>{
         if(deleteforstudent==="true"){
-            let result = await fetch(`http://localhost:5000/delete/${id}`,{
+            let result = await fetch(`https://hostelcomplaintportalbackend1.onrender.com/delete/${id}`,{
               method:"delete"
             });
             result = await result.json();
           //   console.log(result);
           }else{
-            let result = await fetch(`http://localhost:5000/update/${id}`,{
+            let result = await fetch(`https://hostelcomplaintportalbackend1.onrender.com/update/${id}`,{
                 method:"put",
                 body:await JSON.stringify({deleteforadmin}),
                 headers:{
@@ -48,7 +48,7 @@ const ViewComplaint = ()=>{
     const search = async(event)=>{
         let key = event.target.value;
         if(key){
-            let result =await fetch(`http://localhost:5000/search/${key}`);
+            let result =await fetch(`https://hostelcomplaintportalbackend1.onrender.com/search/${key}`);
             result=await result.json();
             if(result){
                 setUsers(result);
